f114ae37f533c0217059a0334af72312c45d9a5f
nei7uj <julien.dille@gmail.com>
2025-04-28 15:37:46 +0200

    Création components

---
+++ my-react-app/src/components/Header/Index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Header = () => {
+    const user = useSelector((state) => state.auth.user);
+    const dispatch = useDispatch();
+  
+    const handleLogout = () => {
+      dispatch(logout());
+    };
+    return (
+        <nav class="main-nav">
+        <a class="main-nav-logo" href="./index.html">
+            <img
+            class="main-nav-logo-image"
+            src="./img/argentBankLogo.png"
+            alt="Argent Bank Logo"
+            />
+            <h1 class="sr-only">Argent Bank</h1>
+        </a>
+        <div>
+            <a class="main-nav-item" href="./sign-in.html">
+            <i class="fa fa-user-circle"></i>
+            Sign In
+            </a>
+        </div>
+        </nav>
+    );
+  };
+  
+  export default Header;
