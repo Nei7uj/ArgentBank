e7775fd73c6bc098de97182cc3bcda38eb6d6e37
nei7uj <julien.dille@gmail.com>
2025-05-05 12:19:38 +0200

    Ajouts Routes, Main + css

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './App.jsx';
+import { store } from './store.jsx';
+import "./main.css";
+import "./pages/sign-in.css";
+import "./components/features.css";
+import "./components/footer.css";
+import "./components/header.css";
+import "./components/hero.css";
+import "./components/user-content.css";
+import "./components/sign-in-content.css";
+import "./pages/user.css";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>,
+  </Provider>,
 )
