d8d406af33e5947736ba8f3586c4983708e96fcd
nei7uj <julien.dille@gmail.com>
2025-05-18 19:04:18 +0200

    Ajouts ligne 2 dans Store.jsx et modifications du nom des images

--- my-react-app/src/Store.jsx
+++ my-react-app/src/Store.jsx
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './Auth/authSlice';
 
 export const store = configureStore({
   reducer: {
--- my-react-app/src/components/Header/Index.jsx
+++ my-react-app/src/components/Header/Index.jsx
@@ -1,30 +1,47 @@
 import React from 'react';
+import logo from '/images/logo.webp'
+import { useSelector, useDispatch } from 'react-redux';
+import { logout } from '../../Auth/authSlice';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
-    const user = useSelector((state) => state.auth.user);
-    const dispatch = useDispatch();
-  
-    const handleLogout = () => {
-      dispatch(logout());
-    };
-    return (
-        <nav class="main-nav">
-        <a class="main-nav-logo" href="./index.html">
-            <img
-            class="main-nav-logo-image"
-            src="./img/argentBankLogo.png"
-            alt="Argent Bank Logo"
-            />
-            <h1 class="sr-only">Argent Bank</h1>
-        </a>
-        <div>
-            <a class="main-nav-item" href="./sign-in.html">
-            <i class="fa fa-user-circle"></i>
-            Sign In
-            </a>
-        </div>
-        </nav>
-    );
+  const user = useSelector((state) => state.auth.user);
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(logout());
   };
-  
-  export default Header;
+  return (
+    <nav className="main-nav">
+      <NavLink className="main-nav-logo" to="/">
+        <img src={logo} className='main-nav-logo-image' alt="Argent Bank Logo" />
+        <h1 className="sr-only">Argent Bank</h1>
+      </NavLink>
+      <div className='user-in'>
+        {user ? (
+          <>
+            <NavLink className="main-nav-item" to="/user">
+              <FontAwesomeIcon icon={faCircleUser} className='fa fa-user-circle'/>
+              {user.userName} 
+            </NavLink>
+            <NavLink className="main-nav-item" to="/" onClick={handleLogout}>
+              <FontAwesomeIcon icon={faRightFromBracket} className='fa fa-sign-out'/>
+              Sign Out 
+            </NavLink>
+          </>
+        ) : (
+          <NavLink className="main-nav-item" to="/sign-in">
+            <FontAwesomeIcon icon={faCircleUser} className='fa fa-user-circle'/>
+            Sign In 
+          </NavLink>
+          )
+        }
+      </div>      
+    </nav>
+  );
+};
+
+export default Header;
